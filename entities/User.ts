a2d149dc7bc8a99201deee66ac65a08ffcb74098
Eric Lake <ericallenlake@gmail.com>
2022-10-31 22:40:12 -0600

    Adds TrackedMonth & User classes

---
+++ entities/User.ts
@@ -0,0 +1,46 @@
+import genUniqueId from "../utils/genUniqueId";
+import TrackedMonth from "./TrackedMonth";
+import Month from "../types/Month";
+
+class User {
+    private name: string;
+    private id: string;
+    private trackedMonths: TrackedMonth[];
+
+    constructor(
+        name: string,
+        trackedMonths: TrackedMonth[],
+    ){
+        this.updateName(name);
+        this.trackedMonths = trackedMonths;
+        this.id = genUniqueId();
+    }
+
+    getId(): string {
+        return this.id;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    updateName(name: string){
+        this.name = name;
+    }
+
+    getTrackedMonths(): TrackedMonth[] {
+        return this.trackedMonths;
+    }
+
+    getTrackedMonthByDate(
+        month: Month,
+        year: number,
+    ): TrackedMonth {
+        return this
+            .getTrackedMonths()
+            .find(trackedMonth => (trackedMonth.getMonth() === month && trackedMonth.getYear() === year));
+    }
+
+}
+
+export default User;
